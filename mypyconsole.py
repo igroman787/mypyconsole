a8f9f56972192247f37ca8f43a5e723e29994c60
Igroman787 <27614297+igroman787@users.noreply.github.com>
2023-10-13 14:53:12 +0300

    add `debug` mode

--- mypyconsole.py
+++ mypyconsole.py
@@ -19,87 +19,105 @@ class MyPyConsole():
 	ENDC = '\033[0m'
 
 	def __init__(self):
+		self.debug = False
 		self.name = "console"
 		self.color = self.GREEN
-		self.unknownCmd = "Unknown command"
-		self.helloText = "Welcome to the console. Enter 'help' to display the help menu."
+		self.unknown_cmd = "Unknown command"
+		self.hello_text = "Welcome to the console. Enter 'help' to display the help menu."
 		self.startFunction = None
-		self.menuItems = list()
-		self.AddItem("help", self.Help, "Print help text")
-		self.AddItem("clear", self.Clear, "Clear console")
-		self.AddItem("exit", self.Exit, "Exit from application")
+		self.start_function = None
+		self.menu_items = list()
+		self.add_item("help", self.help, "Print help text")
+		self.add_item("clear", self.clear, "Clear console")
+		self.add_item("exit", self.exit, "Exit from application")
 		readline.parse_and_bind("tab: complete")
-		readline.set_completer(self.ReadlineCompleter)
+		readline.set_completer(self.readline_completer)
 	#end define
 
-	def ReadlineCompleter(self, text, state):
-		commands = [item.cmd for item in self.menuItems if item.cmd.startswith(text)]
+	def readline_completer(self, text, state):
+		commands = [item.cmd for item in self.menu_items if item.cmd.startswith(text)]
 		if state < len(commands):
 			return commands[state]
 		return None
 	#end define
-
+	
 	def AddItem(self, cmd, func, desc):
-		item = MyPyConsoleItem(cmd, func, desc)
-		self.menuItems.append(item)
+		self.add_item(cmd, func, desc)
 	#end define
 
-	def UserWorker(self):
+	def add_item(self, cmd, func, desc):
+		item = MyPyConsoleItem(cmd, func, desc)
+		self.menu_items.append(item)
+	#end define
+
+	def user_worker(self):
 		try:
 			result = input(self.color + self.name + "> " + self.ENDC)
 		except KeyboardInterrupt:
-			self.Exit()
+			self.exit()
 		except EOFError:
-			self.Exit()
+			self.exit()
 		return result
 	#end define
 
-	def GetCmdFromUser(self):
-		result = self.UserWorker()
-		resultList = result.split(' ')
-		resultList = list(filter(None, resultList))
-		cmd = self.GetItemFromList(resultList, 0)
-		args = resultList[1:]
-		for item in self.menuItems:
+	def get_cmd_from_user(self):
+		result = self.user_worker()
+		result_list = result.split(' ')
+		result_list = list(filter(None, result_list))
+		cmd = self.get_item_from_list(result_list, 0)
+		args = result_list[1:]
+		for item in self.menu_items:
 			if cmd == item.cmd:
-				try:
+				if self.debug == True:
 					item.func(args)
-				except Exception as err:
-					print("{RED}Error: {err}{ENDC}".format(RED=self.RED, ENDC=self.ENDC, err=err))
+				else:
+					self._try(item.func, args)
 				print()
 				return
-		print(self.unknownCmd)
+		print(self.unknown_cmd)
+	#end define
+	
+	def _try(self, func, args):
+		try:
+			func(args)
+		except Exception as err:
+			print("{RED}Error: {err}{ENDC}".format(RED=self.RED, ENDC=self.ENDC, err=err))
 	#end define
 
-	def Help(self, args=None):
+	def help(self, args=None):
 		indexList = list()
-		for item in self.menuItems:
+		for item in self.menu_items:
 			index = len(item.cmd)
 			indexList.append(index)
 		index = max(indexList) + 1
-		for item in self.menuItems:
+		for item in self.menu_items:
 			cmd = item.cmd.ljust(index)
 			print(cmd, item.desc)
 	#end define
 
-	def Clear(self, args=None):
+	def clear(self, args=None):
 		os.system("clear")
 	#end define
 
-	def Exit(self, args=None):
+	def exit(self, args=None):
 		print("Bye.")
 		sys.exit()
 	#end define
-
+	
 	def Run(self):
-		print(self.helloText)
-		if self.startFunction:
-			self.startFunction()
-		while True:
-			self.GetCmdFromUser()
+		self.start_function = self.startFunction
+		self.run()
 	#end define
 
-	def GetItemFromList(self, data, index):
+	def run(self):
+		print(self.hello_text)
+		if self.start_function:
+			self.start_function()
+		while True:
+			self.get_cmd_from_user()
+	#end define
+
+	def get_item_from_list(self, data, index):
 		try:
 			return data[index]
 		except: pass
